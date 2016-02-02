8618763a385a026b22acabde80a459944d0f4c80
RoohSouza <roohsouzadesign@live.com>
2016-02-01 23:59:43 -0800

    Route

--- Route.php
+++ Route.php
@@ -2,9 +2,16 @@
 
 class Route{
 
+  private function error(){
+    echo "Route not found";
+  }
+
   public function get($url, $ctrl)
   {
-    if(isset($_GET['url']))
+    if(!isset($_GET['url']))
+        $_GET['url'] = "/";
+
+    if($url == $_GET['url'])
     {
       $ctrl = explode('#', $ctrl);
       $action = $ctrl[1];
@@ -12,6 +19,8 @@ class Route{
       $ctrl = "\App\Controllers\\$class";
       $controle = new $ctrl();
     }
+    else
+      $this->error();
   }
 
 }
