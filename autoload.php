32bf88adbea714138127e09dcd12fa12cf6054ba
RoohSouza <roohsouzadesign@live.com>
2016-02-01 23:10:57 -0800

    Autoload Update

--- autoload.php
+++ autoload.php
@@ -1,9 +1,6 @@
 <?php
 
-function auto_load($classe)
-{
+spl_autoload_register(function($classe){
   $arquivo =  "$classe.php";
   require_once($arquivo);
-}
-
-spl_autoload_register("auto_load");
+});
